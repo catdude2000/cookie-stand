a183c1316bdd1ac1f861d12b9d0399a0d285ee77
Michael Pace <michael.g.pace@gmail.com>
2021-12-21 20:57:24 -0600

    deleted some comments

--- js/app.js
+++ js/app.js
@@ -54,7 +54,7 @@ let locationFive = {
   }
 };
 
-// Stores the min/max hourly customers, and the average cookies per customer, in object properties
+
 locationOne.setCust();
 console.log(locationOne);
 
@@ -62,12 +62,13 @@ locationTwo.setCust();
 locationThree.setCust();
 locationFour.setCust();
 locationFive.setCust();
-// Uses a method of that object to generate a random number of customers per hour. Objects/Math/random
+
 function randCust (minCust, maxCust) {
   return Math.floor(Math.random() * (maxCust - minCust) + minCust);
 }
 
 // Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated
+
 // Store the results for each location in a separate array… perhaps as a property of the object representing that location
 // Display the values of each array as unordered lists in the browser
 // Calculating the sum of these hourly totals; your output for each location should look like this:
