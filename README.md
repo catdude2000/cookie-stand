a0092162a1210f4942126d8a615b54a4e476447a
Michael Pace <michael.g.pace@gmail.com>
2021-12-21 19:44:35 -0600

    added commented out lab requirements to js and readme

--- README.md
+++ README.md
@@ -15,3 +15,6 @@ For 201-2.0.  A site for "salmon cookies" sales and management.
 
 * Consider including the answers to your daily journal and submission questions here
 * This is also a good place to reflect on the tools and resources used and learned
+
+
+<!-- // construction of a public-facing page, too. They already have a logo image picked out (below), but Pat is requesting your assistance in the design of the documents, color scheme, fonts, and any additional images -->
--- js/app.js
+++ js/app.js
@@ -1,2 +1,24 @@
 'use strict';
 console.log('app.js file is connected');
+
+
+// Pat’s Salmon Cookies, needs to calculate the number of cookies each location must make every day. The number of cookies to make depends on the hours of operation (6:00 AM to 8:00 PM for all locations) and a few factors unique to each location:
+
+// The minimum number of customers per hour.
+// The maximum number of customers per hour.
+// The average number of cookies purchased per customer.
+
+// Pat will need to be able to add and remove locations from the daily projections report, and Pat will also need to be able to easily modify the input numbers for each location based on day of the week, special events, and other factors.
+
+// Within your javascript file , create separate JS object literals for each shop location that outputs the following to the sales.html file:
+
+// Stores the min/max hourly customers, and the average cookies per customer, in object properties
+// Uses a method of that object to generate a random number of customers per hour. Objects/Math/random
+// Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated
+// Store the results for each location in a separate array… perhaps as a property of the object representing that location
+// Display the values of each array as unordered lists in the browser
+// Calculating the sum of these hourly totals; your output for each location should look like this:
+
+// Seattle
+// 6am: 16 cookies
+// 7am: 20 cookies
