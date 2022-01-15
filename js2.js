1db4d88c98e9c824d509a3b6102762495be72d5c
Michael Pace <michael.g.pace@gmail.com>
2022-01-15 16:22:18 -0600

    all working

--- js2.js
+++ js2.js
@@ -1,6 +1,5 @@
 'use strict';
 console.log('app.js file is connected');
-
 function Location(place, minCust, maxCust, avgSales, hourlySold, custPerHour, totalCookieSum) {
   this.place = place;
   this.minCust = minCust;
@@ -10,42 +9,30 @@ function Location(place, minCust, maxCust, avgSales, hourlySold, custPerHour, to
   this.custPerHour = custPerHour;
   this.totalCookieSum = totalCookieSum;
 }
-
 const locationOne = new Location('Seattle', 23, 65, 6.3, [], [], 0);
-
 const locationTwo = new Location('Tokyo', 3, 24, 1.2, [], [], 0);
-
 const locationThree = new Location('Dubai', 11, 38, 3.7, [], [], 0);
-
 const locationFour = new Location('Paris', 20, 38, 2.3, [], [], 0);
-
 const locationFive = new Location('Lima', 2, 16, 4.6, [], [], 0);
-
-
 function randCust (minCust, maxCust) {
   return Math.floor(Math.random() * (maxCust - minCust) + minCust); //function used in each object to create randomized numbers of customers
 }
-
-
 Location.prototype.setCust = function(){
   for(let i = 0; i < 14; i++){
     this.custPerHour.push(randCust(this.minCust, this.maxCust)); //fills custPerHour array with numbers using randCust function
     // console.log('custperhour', this.custPerHour);
   }
 };
-
 Location.prototype.setCookPerHour = function(){
   for(let i = 0; i < 14; i++){
     this.hourlySold.push(Math.floor(this.custPerHour[i] * this.avgSales)); //uses set avg sales with already randomized to give number of cookies sold per hour
   }
 };
-
 Location.prototype.setTotal = function() {
   for (let i = 0; i < this.hourlySold.length; i++) { //adds up randomized hourly sold for a total (behind the scenes)
     this.totalCookieSum += this.hourlySold[i];
   }
 };
-
 // let locationOne = { //creates first object
 //   place: 'Seattle',
 //   minCust: 23,
@@ -73,8 +60,6 @@ Location.prototype.setTotal = function() {
 //     // console.log('sum', sum);
 //   }
 // };
-
-
 locationOne.setCust(); //activates setcust function for location inside object
 locationOne.setCookPerHour(); //" setCookPerHour "
 locationOne.setTotal(); //" setTotal "
@@ -90,14 +75,9 @@ locationFour.setTotal();
 locationFive.setCust();
 locationFive.setCookPerHour();
 locationFive.setTotal();
-
-
 const timeArray = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm']; //array needed to display set data on page
-
-
 // let soldList = document.getElementById('stList'); //links soldList to the stList tag in html
 // console.log('soldlist', soldList);
-
 // for(let i = 0; i< popArray.length; i++){ //iterates through array of location objects, runs 5 times
 //   for(let j = 0; j < timeArray.length; j++){ //iterates through array of set times for each object in popArray, runs 15 times
 //     let soldListLi = document.createElement('li'); //creates list items (li's) in soldListLi
@@ -108,79 +88,55 @@ const timeArray = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am',
 //   storeTotal.textContent = 'Total: ' + popArray[i].totalCookieSum; // creates text for li
 //   soldList.appendChild(storeTotal); //puts text in li
 // }
-
-
 Location.prototype.header = function() {
   let locTable = document.getElementById('stList');
   let headerRow = document.createElement('tr');
   headerRow.textContent = 'Location';
-
-
   for(let i = 0; i < timeArray.length; i++){
     let timeData = document.createElement('td');
     timeData.textContent = timeArray[i];
     headerRow.appendChild(timeData);
     // console.log('td', timeData);
   }
-
   let totals = document.createElement('td'); //adds text to end of table
   totals.textContent = 'totals';
   headerRow.appendChild(totals);
-
   locTable.appendChild(headerRow);
-
 };
-
 Location.prototype.footer = function(){
   let locTable = document.getElementById('stList');
+  // console.log('lotab', locTable);
   let footerRow= document.createElement('tr');
   footerRow.textContent = 'Totals';
 
-  let totalOfTotals =0;
+
   for(let j = 0; j < timeArray.length; j++){
-    let hourlyTotal = 0;
+  // console.log(allLocals[1]);
+    let hourTotals = document.createElement('td');
     for(let i = 0; i < allLocals.length; i++){
-      hourlyTotal +=allLocals[i].hourlySold[j];
-      totalOfTotals += allLocals[i].hourlySold[j];
+      hourTotals.textContent +=hourlySold[j];
     }
-    let totalHourCell= document.createElement('td');
-    totalHourCell.textContent= hourlyTotal;
-    footerRow.appendChild(totalHourCell);
+    // hourTotals.textContent = (locationOne.hourlySold[j] + locationTwo.hourlySold[j] + locationThree.hourlySold[j]
+    //    + locationFour.hourlySold[j] + locationFive.hourlySold[j]);
+
+
+    // console.log('hourtotals', hourTotals);
+    footerRow.appendChild(hourTotals);
+
   }
-
-
-
-  console.log('toft',totalOfTotals);
   let totalTotals = document.createElement('td');
-  console.log('tottals', totalTotals);
-  totalTotals.textContent = totalOfTotals;
+
+  totalTotals.textContent = locationOne.totalCookieSum + locationTwo.totalCookieSum + locationThree.totalCookieSum + locationFour.totalCookieSum + locationFive.totalCookieSum;
   footerRow.appendChild(totalTotals);
-  locTable.appendChild(footerRow);
-};
 
-// };
+  Location.prototype.render = function() {
+    locRow.appendChild(allStoreDayTotals);
+    //this is where totals are coming from
 
-Location.prototype.render = function() {
-  let locTable = document.getElementById('stList');
-  let locRow = document.createElement('tr');
-  let nameCell = document.createElement('td');
-  nameCell.textContent = this.place;
-  locRow.appendChild(nameCell);
-  //get hourly cookie totals for render
-  for(let i = 0; i < timeArray.length; i++){
-    let hourlyData = document.createElement('td');
-    hourlyData.textContent = this.hourlySold[i];
-    locRow.appendChild(hourlyData);
-  }
-  let allStoreDayTotals = document.createElement('td'); //adds sums to end of table
-  allStoreDayTotals.textContent = this.totalCookieSum;
-  locRow.appendChild(allStoreDayTotals);
-  //this is where totals are coming from
+    locTable.appendChild(locRow); //putting row onto table
 
-  locTable.appendChild(locRow); //putting row onto table
-
-};
-
+  };
+}
 
 let allLocals = [locationOne, locationTwo, locationThree, locationFour, locationFive];
 
@@ -224,7 +180,7 @@ function handleSubmit(event){
   addPlace.setCust();
   addPlace.setCookPerHour();
   addPlace.setTotal();
-  // addPlace.render();
+  addPlace.render();
   console.log(addPlace);
 
   // if(!event.target.place.value || !event.target.minCust.value || !event.target.maxCust.value || !event.target.avgSales.value){
@@ -239,26 +195,3 @@ function handleSubmit(event){
 
 formElement.addEventListener('submit', handleSubmit);
 newTotal();
-//add constructor to build objects
-// function addStore(place, minCust, maxCust, avgSales){
-//   this.place = place;
-//   this.minCust = mincust;
-//   this.maxCust = maxCust;
-//   this.avgSales = avgSales;
-// }
-//add a prototype method to build element for render method
-// addStore.prototype.render = function(){
-//  let listItem = document.createElement('li');
-//  listItem.innerHTML = '<img width="" height="" src="images/ +this.newPlace' >
-
-//  return listItem;
-// };
-
-
-//render all
-// let renderAllPlaces = function(){
-//   for(let i =0; i < allPlaces.length; i++){
-//     //call prototype to build our html and append li to page
-//     appendChild(allPlaces[i].render());
-//   }
-// }
